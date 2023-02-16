6e35c2231c57c0b62056f9f86327037c711157e3
maria <maria.benito.lasenia@gmail.com>
2023-02-16 14:06:41 +0000

    Initial commit

---
+++ src/Joke.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Challenge1 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Here is a joke: ");
+    }
+
+}
